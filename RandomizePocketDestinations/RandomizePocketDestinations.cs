a38cdb7dd30a7214829a3e3c6b5270e1f3e9a2c1
Nicholas Solin <solin.nick@gmail.com>
2018-05-28 18:23:53 -0500

    Commit first build from early this morning

---
+++ RandomizePocketDestinations/RandomizePocketDestinations.cs
@@ -0,0 +1,43 @@
+﻿using RandomizePocketDestinations.EventHandlers;
+using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Events;
+using System.Collections.Generic;
+
+namespace RandomizePocketDestinations
+{
+	[PluginDetails(
+		author = "PatPeter",
+		name = "RandomizePocketDestinations",
+		description = "Randomizes pocket dimension locations.",
+		id = "patpeter.randomizepocketdestinations",
+		version = "1.0-build1",
+		SmodMajor = 2,
+		SmodMinor = 0,
+		SmodRevision = 0
+		)]
+	class RandomizePocketDestinationsPlugin : Plugin
+	{
+		public Dictionary<Classes, Vector> SCPSpawnPoints = new Dictionary<Classes, Vector>();
+
+		public override void OnEnable()
+		{
+			this.Info("Randomize Pocket Destinations has loaded :)");
+			this.Info("randomize_pocket_destinations value: " + this.GetConfigBool("randomize_pocket_destinations"));
+		}
+
+		public override void OnDisable()
+		{
+		}
+
+		public override void Register()
+		{
+			// Register Events
+			this.AddEventHandler(typeof(IEventRoundStart), new RoundStartHandler(this), Priority.Highest);
+			this.AddEventHandler(typeof(IEventPocketDimensionExit), new PocketDimensionExitHandler(this), Priority.Highest);
+			// Register config settings
+			this.AddConfig(new Smod2.Config.ConfigSetting("randomize_pocket_destinations", false, Smod2.Config.SettingType.BOOL, true, "Randomzies pocket destinations."));
+		}
+	}
+}
---
+++ RandomizePocketDestinations/RoundStartEventHandler.cs
@@ -0,0 +1,58 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Events;
+using Smod2.Config;
+
+namespace RandomizePocketDestinations.EventHandlers
+{
+    class RoundStartHandler : IEventRoundStart
+    {
+		private RandomizePocketDestinationsPlugin plugin;
+
+		public RoundStartHandler(Plugin plugin)
+		{
+			this.plugin = (RandomizePocketDestinationsPlugin) plugin;
+		}
+
+		public void OnRoundStart(Server server)
+		{
+			foreach (Player player in server.GetPlayers())
+			{
+				switch (player.Class.ClassType)
+				{
+					case Classes.SCP_049:
+					case Classes.SCP_096:
+					case Classes.SCP_106:
+					//case Classes.SCP_173:
+						this.plugin.SCPSpawnPoints.Add(player.Class.ClassType, player.GetPosition());
+						break;
+				}
+			}
+			plugin.Info("SCPSpawnPoints SIZE " + this.plugin.SCPSpawnPoints.Count);
+		}
+	}
+
+	class PocketDimensionExitHandler : IEventPocketDimensionExit
+	{
+		private RandomizePocketDestinationsPlugin plugin;
+
+		public PocketDimensionExitHandler(Plugin plugin)
+		{
+			this.plugin = (RandomizePocketDestinationsPlugin) plugin;
+		}
+
+		public void OnPocketDimensionExit(Vector[] possibleExits, out Vector[] possibleExitsOutput)
+		{
+			//ConfigSetting randomziePocketDestinations = ConfigManager.Manager.ResolveSetting(this.plugin, "randomize_pocket_destinations");
+			if (this.plugin.GetConfigBool("randomize_pocket_destinations") && this.plugin.SCPSpawnPoints.Count > 0)
+			{
+				Vector[] destinations = new Vector[this.plugin.SCPSpawnPoints.Count];
+				this.plugin.SCPSpawnPoints.Values.CopyTo(destinations, 0);
+				possibleExitsOutput = destinations;
+			} else
+			{
+				possibleExitsOutput = possibleExits;
+			}
+		}
+	}
+}
